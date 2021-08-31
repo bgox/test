93696d2910ef8f65d312139571852cf015323fb0
刘军 <30679801+bgox@users.noreply.github.com>
2021-09-01 00:17:06 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+(function (global, factory) {
+  if (typeof module !== "undefined" && module.exports) {
+    // CMD
+    module.exports.__dataverseNftCrawler = factory();
+  } else {
+    global.__dataverseNftCrawler = factory();
+    global.addEventListener("RecivedDataverseMsg",function(e){
+      console.log("RecivedDataverseMsg",e);
+      console.log(global.__dataverseNftCrawler.opensea());
+      console.log(global.__dataverseNftCrawler.superrare());
+    });
+    global.postMessage("SendMsgToDataverse","*",global.__dataverseNftCrawler);
+  }
+})(window, function () {
+  var __dataverseNftCrawler = {
+    opensea:function(){
+      console.log("opensea");
+    },
+    superrare:function(){
+      console.log("superrare");
+    }
+  }
+
+  return __dataverseNftCrawler;
+});
